ddd41ca46712664628261ee2c95da88e26489f2e
Sergio Vera <sergio.vera@icloud.com>
2021-09-24 13:37:53 +0000

    mul

---
+++ mul.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * _mul - multiply the second top element with the top element of the stack
+ * @stack: top of the stack
+ * @line: line number
+ * Return: Nothing
+ */
+
+void _mul(stack_t **stack, unsigned int line)
+{
+	stack_t *temp;
+
+	if (!(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	(*stack)->next->n *= (*stack)->n;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
+}
